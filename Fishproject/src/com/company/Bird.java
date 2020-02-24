43f0d2e4e238ffec04f3d283191b70a7e5fb3e09
RenuJaishankar <Renu.Jaishankar@yahoo.com>
2020-02-24 14:30:21 -0500

    Bird class added

---
+++ Fishproject/src/com/company/Bird.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Bird {
+    private int noOfFeather;
+    private int noofeyes;
+
+    public int getNoofeyes() {
+        return noofeyes;
+    }
+
+    public int getNoOfFeather() {
+        return noOfFeather;
+
+    }
+
+    public void setNoOfFeather(int noOfFeather) {
+        this.noOfFeather = noOfFeather;
+    }
+
+    public void setNoofeyes(int noofeyes) {
+        this.noofeyes = noofeyes;
+    }
+public Bird(int noOfFeather,int oofeyes){
+    setNoOfFeather(noOfFeather);
+    setNoofeyes(noofeyes);
+}
+
+}
--- Fishproject/src/com/company/Main.java
+++ Fishproject/src/com/company/Main.java
@@ -8,10 +8,20 @@ public class Main {
             System.out.println("I think your fish is dead");
         }
     }
+    static void featherChecker(int feathers){
+        if (feathers >=24){
+            System.out.println("The bird has enough feathers");
+        } else {
+            System.out.println("THe bird has less feathers");
+        }
+    }
+
     public static void main(String[] args) {
 	// write your code here
         Fish myFish = new Fish(7,4);
         gulpChecker(myFish.getNumOfGulps());
+        Bird myBird = new Bird(24,2);
+        featherChecker(myBird.getNoOfFeather());
 
     }
 }
